ef60febe9a096c88af3f0b928ea0537fcc5c5458
celine-y <16328670+celine-y@users.noreply.github.com>
2020-01-11 00:41:34 -0500

    routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
+import {
+  BrowserRouter as Router,
+  Route
+ } from 'react-router-dom';
+import Navigation from '../Navigation';
+
+import HomePage from '../Home';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
-export default Home;
+export default App;
