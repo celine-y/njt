289c8b87cf16ef70ca37f94b09370936c21463c3
celine-y <16328670+celine-y@users.noreply.github.com>
2020-01-23 14:43:21 -0500

    add firebase in project

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as ROUTES from 'constants/routes';
 
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
+// Firebase
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 // pages for this product
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
@@ -15,13 +18,15 @@ import SignupPage from "views/SignupPage/SignupPage.js";
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route exact path={ROUTES.HOME} component={LandingPage} />
-      <Route path={ROUTES.ACCOUNT} component={ProfilePage} />
-      <Route path={ROUTES.LOGIN} component={LoginPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignupPage} />
-    </Switch>
-  </Router>,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <Router history={hist}>
+      <Switch>
+        <Route exact path={ROUTES.HOME} component={LandingPage} />
+        <Route path={ROUTES.ACCOUNT} component={ProfilePage} />
+        <Route path={ROUTES.LOGIN} component={LoginPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignupPage} />
+      </Switch>
+    </Router>
+  </FirebaseContext.Provider>,
   document.getElementById("root")
 );
