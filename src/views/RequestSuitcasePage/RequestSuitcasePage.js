4db6433411cfe2482c20f94cad9674119903d079
michelleecheng <chengmichy@gmail.com>
2020-02-13 09:14:26 -0500

    Formatting changes

--- src/views/RequestSuitcasePage/RequestSuitcasePage.js
+++ src/views/RequestSuitcasePage/RequestSuitcasePage.js
@@ -226,10 +226,20 @@ export default function RequestSuitcasePage(props) {
                   <h6>I accept the terms of the Traveller Agreement </h6>
                   <p>
                   1) By submitting this form and accepting a suitcase, the traveller assumes full responsibility for the suitcase. The traveller must unpack their suitcase, inspect the contents and repack them. The traveller must not add to or remove anything from the suitcase.
+                  </p>
+                  <p>
                   2)The suitcase must be taken to a small clinic and cannot be delivered to anyone other then the destination clinic. The doctor receiving the suitcase must sign a letter indicating delivery.
+                  </p>
+                  <p>
                   3) Not Just Tourists is a non-political organization, no claims or implications shall be made for the delivery of the suitcases other than for humanitarian purposes. The traveller agrees to do so with impartiality, neutrality, independence and voluntary service. The traveller will not discriminate on the basis of nationality, race, religion, gender or political affiliation.
+                  </p>
+                  <p>
                   4) Not Just Tourists will not reimburse travellers for additional baggage charges, delays or any other damages resulting from delivering a suitcase.The traveller is responsible for knowing applicable laws and regulations in delivering medical supplies to the host country.
+                  </p>
+                  <p>
                   5) The traveller acknowledges and agrees to indemnify and hold harmless Not Just Tourists Toronto together with all respective directors, officers, volunteers, employees and agents from all liability, all manner of actions, causes of action, suits, demands and costs, including but not limited to those brought by a third party arising from any actions related in any way to accepting/delivering a suitcase.
+                  </p>
+                  <p>
                   6) Any photos received by Not Just Tourists become the property of the organization and can be used for marketing and promotional purposes.
                   </p>
                   <div className={wrapperDiv}>
