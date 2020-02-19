49031cafa894f3fa29b50cfa3153b3ef1b7e1bee
celine-y <16328670+celine-y@users.noreply.github.com>
2020-02-18 21:07:25 -0500

    display email on profile page

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -16,7 +16,7 @@ import NavPills from "components/NavPills/NavPills.js";
 import Parallax from "components/Parallax/Parallax.js";
 
 // Authorization
-import { withAuthorization } from 'components/Session';
+import { AuthUserContext, withAuthorization } from 'components/Session';
 
 import profile from "assets/img/faces/christian.jpg";
 
@@ -45,162 +45,166 @@ function ProfilePage(props) {
   );
   const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   return (
-    <div>
-      <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
-      <div className={classNames(classes.main, classes.mainRaised)}>
+    <AuthUserContext.Consumer>
+      { authUser => (
         <div>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={6}>
-                <div className={classes.profile}>
-                  <div>
-                    <img src={profile} alt="..." className={imageClasses} />
-                  </div>
-                  <div className={classes.name}>
-                    <h3 className={classes.title}>Christian Louboutin</h3>
-                    <h6>DESIGNER</h6>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-twitter"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-instagram"} />
-                    </Button>
-                    <Button justIcon link className={classes.margin5}>
-                      <i className={"fab fa-facebook"} />
-                    </Button>
-                  </div>
+          <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
+          <div className={classNames(classes.main, classes.mainRaised)}>
+            <div>
+              <div className={classes.container}>
+                <GridContainer justify="center">
+                  <GridItem xs={12} sm={12} md={6}>
+                    <div className={classes.profile}>
+                      <div>
+                        <img src={profile} alt="..." className={imageClasses} />
+                      </div>
+                      <div className={classes.name}>
+                        <h3 className={classes.title}>{authUser.email}</h3>
+                        <h6>DESIGNER</h6>
+                        <Button justIcon link className={classes.margin5}>
+                          <i className={"fab fa-twitter"} />
+                        </Button>
+                        <Button justIcon link className={classes.margin5}>
+                          <i className={"fab fa-instagram"} />
+                        </Button>
+                        <Button justIcon link className={classes.margin5}>
+                          <i className={"fab fa-facebook"} />
+                        </Button>
+                      </div>
+                    </div>
+                  </GridItem>
+                </GridContainer>
+                <div className={classes.description}>
+                  <p>
+                    An artist of considerable range, Chet Faker — the name taken by
+                    Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
+                    and records all of his own music, giving it a warm, intimate
+                    feel with a solid groove structure.{" "}
+                  </p>
                 </div>
-              </GridItem>
-            </GridContainer>
-            <div className={classes.description}>
-              <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
-              </p>
+                <GridContainer justify="center">
+                  <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                    <NavPills
+                      alignCenter
+                      color="primary"
+                      tabs={[
+                        {
+                          tabButton: "Studio",
+                          tabIcon: Camera,
+                          tabContent: (
+                            <GridContainer justify="center">
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={studio1}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={studio2}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={studio5}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={studio4}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                            </GridContainer>
+                          )
+                        },
+                        {
+                          tabButton: "Work",
+                          tabIcon: Palette,
+                          tabContent: (
+                            <GridContainer justify="center">
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={work1}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={work2}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={work3}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={work4}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={work5}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                            </GridContainer>
+                          )
+                        },
+                        {
+                          tabButton: "Favorite",
+                          tabIcon: Favorite,
+                          tabContent: (
+                            <GridContainer justify="center">
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={work4}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={studio3}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                              <GridItem xs={12} sm={12} md={4}>
+                                <img
+                                  alt="..."
+                                  src={work2}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={work1}
+                                  className={navImageClasses}
+                                />
+                                <img
+                                  alt="..."
+                                  src={studio1}
+                                  className={navImageClasses}
+                                />
+                              </GridItem>
+                            </GridContainer>
+                          )
+                        }
+                      ]}
+                    />
+                  </GridItem>
+                </GridContainer>
+              </div>
             </div>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                <NavPills
-                  alignCenter
-                  color="primary"
-                  tabs={[
-                    {
-                      tabButton: "Studio",
-                      tabIcon: Camera,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio2}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={studio5}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio4}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Work",
-                      tabIcon: Palette,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work3}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work4}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work5}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    },
-                    {
-                      tabButton: "Favorite",
-                      tabIcon: Favorite,
-                      tabContent: (
-                        <GridContainer justify="center">
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work4}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio3}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                          <GridItem xs={12} sm={12} md={4}>
-                            <img
-                              alt="..."
-                              src={work2}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={work1}
-                              className={navImageClasses}
-                            />
-                            <img
-                              alt="..."
-                              src={studio1}
-                              className={navImageClasses}
-                            />
-                          </GridItem>
-                        </GridContainer>
-                      )
-                    }
-                  ]}
-                />
-              </GridItem>
-            </GridContainer>
           </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
+      )}
+    </AuthUserContext.Consumer>
   );
 }
 
