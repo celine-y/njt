e12e0756c9dc433eddb97c808cb2db9980846034
michelleecheng <chengmichy@gmail.com>
2020-02-20 21:31:17 -0500

    Fixed up page

--- src/views/SubmitClinicPage/SubmitClinicPage.js
+++ src/views/SubmitClinicPage/SubmitClinicPage.js
@@ -34,14 +34,10 @@ import Check from "@material-ui/icons/Check";
 // material-ui components
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
-
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
 //import styles2 from "assets/jss/material-kit-react/customCheckboxRadioSwitch.js";
-
-import image from "assets/img/bg7.jpg";
-
+import image from "assets/img/bg2.jpg";
 import * as ROUTES from 'constants/routes';
-
 const useStyles = makeStyles(styles);
 
 export default function SubmitClinicPage(props) {
@@ -85,40 +81,36 @@ export default function SubmitClinicPage(props) {
       >
       <div className={classes.container}>
         <GridContainer justify="center">
-          <GridItem xs={24} sm={24} md={8}>
+          <GridItem xs={24} sm={24} md={11}>
             <Card className={classes[cardAnimaton]}>
               <form className={classes.form}>
                 <CardHeader color="primary" className={classes.cardHeader}>
-                  <h4>Request a Suitcase</h4>
+                  <h4>Submit a Clinic</h4>
                   <div className={classes.socialLine}>
                   </div>
                 </CardHeader>
                 <CardBody>
-                  <p>Please select which chapter you belong to</p>
-                    <div>
-                      <CustomDropdown
-                        buttonText="Select"
-                        dropdownList={[
-                          "Calgary",
-                          "Halifax",
-                          "London",
-                          "Niagara",
-                          "Toronto",
-                          "Waterloo",
-                        ]}
-                      />
-                    </div>
-                    <br />
-                    <div>
-                    <p>Please select a date and time for available luggage pick-up</p>
-                      <FormControl fullWidth>
-                        <Datetime
-                          inputProps={{ placeholder: "Select date and time..." }}
-                        />
-                      </FormControl>
-                    </div>
+                <p>Please fill out the following information regarding the clinic</p>
                   <CustomInput
-                    labelText="Destination..."
+                    labelText="Clinic Name..."
+                    id="text"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                    inputProps={{
+                      type: "text",
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Icon className={classes.inputIconsColor}>
+                            lock_outline
+                          </Icon>
+                        </InputAdornment>
+                      ),
+                      autoComplete: "off"
+                    }}
+                  />
+                  <CustomInput
+                    labelText="Street Name..."
                     id="first"
                     formControlProps={{
                       fullWidth: true
@@ -135,7 +127,7 @@ export default function SubmitClinicPage(props) {
                     }}
                   />
                   <CustomInput
-                    labelText="Flight Number..."
+                    labelText="Street Number..."
                     id="email"
                     formControlProps={{
                       fullWidth: true
@@ -150,7 +142,7 @@ export default function SubmitClinicPage(props) {
                     }}
                   />
                   <CustomInput
-                    labelText="What are you willing to carry..."
+                    labelText="Country..."
                     id="pass"
                     formControlProps={{
                       fullWidth: true
@@ -168,7 +160,7 @@ export default function SubmitClinicPage(props) {
                     }}
                   />
                   <CustomInput
-                    labelText="Why do you want to take a suitcase?"
+                    labelText="Province..."
                     id="text"
                     formControlProps={{
                       fullWidth: true
@@ -186,25 +178,7 @@ export default function SubmitClinicPage(props) {
                     }}
                   />
                   <CustomInput
-                    labelText="How did you hear about us?"
-                    id="text"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                    inputProps={{
-                      type: "password",
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <Icon className={classes.inputIconsColor}>
-                            lock_outline
-                          </Icon>
-                        </InputAdornment>
-                      ),
-                      autoComplete: "off"
-                    }}
-                  />
-                  <CustomInput
-                      labelText="Comments/Questions?"
+                      labelText="Additional Comments"
                       id="text"
                       formControlProps={{
                         fullWidth: true
@@ -221,60 +195,6 @@ export default function SubmitClinicPage(props) {
                         autoComplete: "off"
                       }}
                     />
-                  <br />
-                  <br />
-                  <h6>I accept the terms of the Traveller Agreement </h6>
-                  <p>
-                  1) By submitting this form and accepting a suitcase, the traveller assumes full responsibility for the suitcase. The traveller must unpack their suitcase, inspect the contents and repack them. The traveller must not add to or remove anything from the suitcase.
-                  </p>
-                  <p>
-                  2)The suitcase must be taken to a small clinic and cannot be delivered to anyone other then the destination clinic. The doctor receiving the suitcase must sign a letter indicating delivery.
-                  </p>
-                  <p>
-                  3) Not Just Tourists is a non-political organization, no claims or implications shall be made for the delivery of the suitcases other than for humanitarian purposes. The traveller agrees to do so with impartiality, neutrality, independence and voluntary service. The traveller will not discriminate on the basis of nationality, race, religion, gender or political affiliation.
-                  </p>
-                  <p>
-                  4) Not Just Tourists will not reimburse travellers for additional baggage charges, delays or any other damages resulting from delivering a suitcase.The traveller is responsible for knowing applicable laws and regulations in delivering medical supplies to the host country.
-                  </p>
-                  <p>
-                  5) The traveller acknowledges and agrees to indemnify and hold harmless Not Just Tourists Toronto together with all respective directors, officers, volunteers, employees and agents from all liability, all manner of actions, causes of action, suits, demands and costs, including but not limited to those brought by a third party arising from any actions related in any way to accepting/delivering a suitcase.
-                  </p>
-                  <p>
-                  6) Any photos received by Not Just Tourists become the property of the organization and can be used for marketing and promotional purposes.
-                  </p>
-                  <div className={wrapperDiv}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          tabIndex={-1}
-                          onClick={() => handleToggle(21)}
-                          checkedIcon={<Check className={classes.checkedIcon} />}
-                          icon={<Check className={classes.uncheckedIcon} />}
-                          classes={{ checked: classes.checked }}
-                        />
-                      }
-                      classes={{ label: classes.label }}
-                      label="Yes"
-                    />
-                  </div>
-                  <div className={wrapperDiv}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          tabIndex={-1}
-                          onClick={() => handleToggle(22)}
-                          checked={
-                            checked.indexOf(22) !== -1 ? true : false
-                          }
-                          checkedIcon={<Check className={classes.checkedIcon} />}
-                          icon={<Check className={classes.uncheckedIcon} />}
-                          classes={{ checked: classes.checked }}
-                        />
-                      }
-                      classes={{ label: classes.label }}
-                      label="No"
-                    />
-                  </div>
                 </CardBody>
                 <CardFooter className={classes.cardFooter}>
                   <Button color="primary" size="lg"
