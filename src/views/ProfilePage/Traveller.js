7e29ac8957c3fa4ab9efb8341d0bc8326310bbe0
celine-y <16328670+celine-y@users.noreply.github.com>
2020-02-29 11:39:00 -0500

    col

--- src/views/ProfilePage/Traveller.js
+++ src/views/ProfilePage/Traveller.js
@@ -52,8 +52,8 @@ function Traveller(props) {
                 <GridItem xs={12} sm={12} md={9}>
                   <GridContainer>
                   <GridItem xs={12}>
-                    <div className={classes.name}>
-                      <h3 className={classes.title}>Trips</h3>
+                    <div className={classes.title}>
+                      <h3>Trips</h3>
                     </div>
                   </GridItem>
                   </GridContainer>
