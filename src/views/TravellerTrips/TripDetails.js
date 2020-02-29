4b36060e754e346ba52b9becfb1aefc9e2ada483
13satwant <ss2rai@uwaterloo.ca>
2020-02-29 16:07:44 -0500

    Added Cards to Trip Details

--- src/views/TravellerTrips/TripDetails.js
+++ src/views/TravellerTrips/TripDetails.js
@@ -1,91 +1,34 @@
-import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Component } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
-// @material-ui/icons
-import Camera from "@material-ui/icons/Camera";
-import Palette from "@material-ui/icons/Palette";
-import Favorite from "@material-ui/icons/Favorite";
-
-// core components
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import NavPills from "components/NavPills/NavPills.js";
-import Parallax from "components/Parallax/Parallax.js";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import Button from "components/CustomButtons/Button.js";
-
-import { cardTitle } from "assets/jss/material-kit-react.js";
-
-// Authorization
-import { AuthUserContext, withAuthorization } from 'components/Session';
-
-import profile from "assets/img/faces/BabbuMann.jpg";
-import styles from "assets/jss/material-kit-react/views/profilePage.js";
-
-const useStyles = makeStyles(styles);
-
-const cardStyles = {
-  cardTitle,
-  textCenter: {
-    textAlign: "center"
-  },
-  textRight: {
-    textAlign: "right"
+class Example extends Component {
+  constructor(props) {
+super(props);
+this.toggle = this.toggle.bind(this);
+this.state = { collapse: false };
   }
-};
 
-export default function AdminTrips(props) {
-  const classes = useStyles();
-  const imageClasses = classNames(
-    classes.imgRaised,
-    classes.imgRoundedCircle,
-    classes.imgFluid
-  );
+  toggle() {
+this.setState(state => ({ collapse: !state.collapse }));
+  }
 
-  const { ...rest } = props;
-  return (
-    <div>
-      <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
-      <div className={classNames(classes.main, classes.mainRaised)}>
-        <div>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={6}>
-                <div className={classes.profile}>
-                  <div>
-                    <img src={profile} alt="..." className={imageClasses} />
-                  </div>
-                </div>
-              </GridItem>
-            </GridContainer>
-            <div>
-              {/* filtered cards by chapter */}
-              <Card style={{ width: "50%" }}>
-                <CardBody>
-                  <h4 className={classes.cardTitle}>Traveller Name</h4>
-                  <p>
-                    Traveller's status: requested
-                    </p>
-                  <p>
-                    Country:
-                  </p>
-                  <Button color="primary">View Details</Button>
-                </CardBody>
-              </Card>
-            </div>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-              </GridItem>
-            </GridContainer>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div >
-  );
+  render() {
+return (
+  <div>
+    <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+    <Collapse isOpen={this.state.collapse}>
+      <Card>
+        <CardBody>
+        Anim pariatur cliche reprehenderit,
+         enim eiusmod high life accusamus terry richardson ad squid. Nihil
+         anim keffiyeh helvetica, craft beer labore wes anderson cred
+         nesciunt sapiente ea proident.
+        </CardBody>
+      </Card>
+    </Collapse>
+  </div>
+    );
+  }
 }
+
+export default Example;
