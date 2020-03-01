6f1a3b6b70b7b2b1520f5c2b7fb8a0bea5bfdac3
celine-y <16328670+celine-y@users.noreply.github.com>
2020-03-01 17:58:09 -0500

    using google npm package

--- src/views/SubmitClinicPage/FormSubmitClinic.js
+++ src/views/SubmitClinicPage/FormSubmitClinic.js
@@ -1,15 +1,8 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-
-// @material-ui/icons
-import HomeIcon from '@material-ui/icons/Home';
 
 // core components
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Button from "components/CustomButtons/Button.js";
@@ -18,101 +11,116 @@ import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardFooter from "components/Card/CardFooter.js";
 import CustomInput from "components/CustomInput/CustomInput.js";
-//gives imaage at the top
+
+//gives image at the top
 import Parallax from "components/Parallax/Parallax.js";
 import classNames from "classnames";
-//creates dropdown
-import CustomDropdown from 'components/CustomDropdown/CustomDropdown.js';
-import Badge from 'components/Badge/Badge.js';
 
 // material-ui components
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
-import image from "assets/img/bg2.jpg";
+
+import { useFormFields } from "libs/hooksLibs";
+
+import AddressItem from 'components/Address/AddressItem';
+import AddressInput from 'components/Address/AddressInput';
 import * as ROUTES from 'constants/routes';
 
+import {Client} from "@googlemaps/google-maps-services-js";
 
 const useStyles = makeStyles(styles);
 export default function FormSubmitClinic(props){
   const classes = useStyles();
   const { ...rest } = props;
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+  const [cardAnimaton, setCardAnimation] = useState("cardHidden");
   setTimeout(function() {
     setCardAnimation("");
   }, 700);
 
+  const initialState = {
+    clinicName: "",
+    street: "",
+    country: "",
+    province: "",
+    comments: ""
+  }
+
+  const [fields, handleFieldChange, setFields] = useFormFields(initialState);
+  const [error, setError] = useState(null);
+
+  const client = new Client({});
+
+  function changeQuery(q){
+      if(q) {
+        client
+        .textSearch({
+          headers: {
+            "Access-Control-Allow-Origin": "http://localhost:3000/"
+          },
+          params: {
+            query: [q],
+            fields: 'place_id',
+            key: process.env.GOOGLE_MAPS_API_KEY,
+          },
+          timeout: 1000 // milliseconds
+        })
+        .then(r => {
+          console.log(r.data.results);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+      }
+  }
+
+
+  function onSubmit(e) {
+    console.log(fields);
+    e.preventDefault();
+  }
+
+
+
   return (
-    <GridContainer justify="center">
-      <GridItem xs={24} sm={24} md={11}>
-        <Card className={classes[cardAnimaton]}>
-          <form className={classes.form}>
-            <CardHeader color="primary" className={classes.cardHeader}>
-              <h4>Submit a Clinic</h4>
-              <div className={classes.socialLine}>
-              </div>
-            </CardHeader>
-            <CardBody>
-            <p>Please fill out the following information regarding the clinic</p>
-              <CustomInput
-                labelText="Clinic Name..."
-                id="text"
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  type: "text",
-                  autoComplete: "off"
-                }}
-              />
-              <CustomInput
-                labelText="Street Name and Number..."
-                id="first"
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  type: "text",
-                }}
-              />
-              <CustomInput
-                labelText="Country..."
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  type: "text",
-                  autoComplete: "on"
-                }}
-              />
-              <CustomInput
-                labelText="Province..."
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  autoComplete: "off"
-                }}
-              />
-              <CustomInput
-                  labelText="Additional Comments"
-                  formControlProps={{
-                    fullWidth: true
-                  }}
-                  inputProps={{
-                    autoComplete: "off"
-                  }}
+      <GridContainer justify="center">
+        <GridItem xs={12}>
+          <Card className={classes[cardAnimaton]}>
+            <form className={classes.form}>
+              <CardHeader color="primary" className={classes.cardHeader}>
+                <h4>Submit a Clinic</h4>
+                <div className={classes.socialLine}>
+                </div>
+              </CardHeader>
+              <CardBody>
+              <p>Please fill out the following information regarding the clinic</p>
+                <AddressItem
+                  labelText="Clinic Name..."
+                  id="clinicName"
+                  onChange={changeQuery}
                 />
-            </CardBody>
-            <CardFooter className={classes.cardFooter}>
-              <Button color="primary" size="lg"
-                href={ROUTES.LOGIN}>
-                Submit
-              </Button>
-            </CardFooter>
-          </form>
-        </Card>
-      </GridItem>
-    </GridContainer>
+                <AddressInput
+                  onChange={handleFieldChange}
+                 />
+                <CustomInput
+                    labelText="Additional Comments"
+                    id="comments"
+                    formControlProps={{
+                      fullWidth: true
+                    }}
+                    inputProps={{
+                      onChange: (e) => handleFieldChange(e),
+                      type: "text"
+                    }}
+                  />
+              </CardBody>
+              <CardFooter className={classes.cardFooter}>
+                <Button color="primary" size="lg"
+                  onClick={onSubmit}>
+                  Submit
+                </Button>
+              </CardFooter>
+            </form>
+          </Card>
+        </GridItem>
+      </GridContainer>
   );
 }
