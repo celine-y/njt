668ae3d3f66bb79e78ead5ef7da7c1f1343e7651
13satwant <ss2rai@uwaterloo.ca>
2020-03-09 00:26:29 -0400

    Trip Details Page

---
+++ src/assets/jss/material-kit-react/dropbutton.js
@@ -0,0 +1,22 @@
+const dropbuttonStyles = {
+     
+    cusButton:{
+       width:'100px',
+       height:'30px'
+    },
+    controlButton:{
+        height:'30px',
+        padding: '4px 52px 8px 10px'
+
+    },
+
+    dropbutton:{
+
+      
+
+    }
+  
+
+}
+
+export default dropbuttonStyles;
